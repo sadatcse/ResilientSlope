db8096aa1207733ae9df8f33ba9787068fe1097f
sadat khan <sadatcse@gmail.com>
2024-04-01 22:23:55 +0600

    service page update

--- src/pages/RecycledPlasticPin​.jsx
+++ src/pages/RecycledPlasticPin​.jsx
@@ -1,17 +1,40 @@
 import React from 'react';
 import { Helmet } from 'react-helmet-async';
 import Title from '../components/Title/Title';
-const RecycledPlasticPin= () => {
+
+const RecycledPlasticPin = () => {
     return (
         <div>
- <Helmet>
-    <title>Recycled Plastic Pin | Resilient Slope LLC</title>
-    <meta name="description" content="Recycled Plastic Pin is an environmentally friendly alternative to traditional metal pins." />
-</Helmet>
-             <Title title="Recycled Plastic Pin"/>
-             <div class="mx-1 mt-2 sm:mx-9 sm:mt-9 bg-slate-500">
-    <h1>Test</h1>
-</div>
+            <Helmet>
+                <title>Recycled Plastic Pin | Resilient Slope LLC</title>
+                <meta name="description" content="Recycled Plastic Pin is an environmentally friendly alternative to traditional metal pins." />
+            </Helmet>
+            <Title title="Recycled Plastic Pin" />
+            <div className="mx-1 mt-2 sm:mx-9 sm:mt-9 ">
+                <div className="flex flex-wrap w-11/12">
+
+                    <div className="w-full sm:w-1/2 p-4">
+                        <div className="mx-auto px-4 sm:px-6 lg:px-8">
+                            <h2 className="text-3xl font-bold text-blue-500 mb-4">What is Recycled Plastic Pin?</h2>
+                            <ul className='list-outside hover:list-inside'>
+                                <li className=" mb-3 ml-5"><span className="dot"></span>Recycled Plastic Pin (RPP) is made from Recycled Plastic Waste, and it is a highly cost-effective solution.</li>
+                                <li className=" mb-3 ml-5"><span className="dot"></span>It is a commercially available materials from multiple vendors all over USA.</li>
+                                <li className=" mb-3 ml-5"><span className="dot"></span>A Single 4”x4” and 10 ft. long RPP replace more than 500 plastic water bottles.</li>
+                                <li className=" mb-3 ml-5"><span className="dot"></span>Using RPP in Slope Repair have significant environmental benefits as it create a market for recycled plastics and reduces the waste plastic volume from the landfill.</li>
+                                <li className=" mb-3 ml-5"><span className="dot"></span>RPP doesn’t degrade when installed into to the ground and require no maintenance.</li>
+                            </ul>
+          
+                        </div>
+                        <div className="my-10 aspect-w-16 aspect-h-12">
+                            <iframe className="w-full h-full" src="https://www.youtube.com/embed/c0DResP-F4I" title="UTA researcher uses recycled plastic to strengthen highways" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerpolicy="strict-origin-when-cross-origin" allowfullscreen></iframe>
+                        </div>
+                    </div>
+
+                    <div className="w-full sm:w-1/2 p-4">
+                        <h1 className="text-center">Hello World</h1>
+                    </div>
+                </div>
+            </div>
         </div>
     );
 };
--- src/pages/Services​.jsx
+++ src/pages/Services​.jsx
@@ -1,16 +1,74 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import { Helmet } from 'react-helmet-async';
 import Title from '../components/Title/Title';
+import AOS from 'aos';
+
 const Services= () => {
+    useEffect(() => {
+        AOS.init(); 
+    }, []);
+    const data = [
+        {
+          title: "Design-Build Services of Landslide Investigation and Repair",
+          start:"1",
+          end:"1200",
+          category_array: [
+            "Investigation,Design and Repair of Failed Slopes using Recycled Plastic Pin",
+            "Erosion Control of Slopes using Vetiver Grass",
+            "Stabilization of Marginal/Steep Slopes using Recycled Plastic Pin or Vetiver Grass"
+          ],
+          image_url: "https://i.ibb.co/yF0zS81/landslide.jpg"
+        },
+        {
+            title: "Design and Analysis",
+            start:"200",
+          end:"1400",
+            category_array: [
+              "Analysis and Design of Slopes, Embankment, Levees and Earth Retaining System using Limit Equilibrium and Finite Element Technique",
+              "Value Engineering and Prepare Alternate Design",
+              "Failure Investigation and Back Analysis using FEM"
+            ],
+            image_url: "https://i.ibb.co/G0d8vdX/analysis.png"
+          },
+          {
+            title: "Non-Destructive and Geophysical Testing",
+            start:"400",
+          end:"1600",
+            category_array: [
+            "Electrical Resistivity Imaging",
+            "Ground Penetration Radar",
+            "MASW",
+            "Instrumentation and Monitoring",
+             "LiDAR Imaging",
+            "Drone Imaging"
+            ],
+            image_url: "https://i.ibb.co/8KNmFzg/Testing.png"
+          }
+      ];
+
     return (
         <div>
-<Helmet>
-    <title>Services | Resilient Slope LLC</title>
-    <meta name="description" content="Explore our range of services offered." />
-</Helmet>
+            <Helmet>
+                <title>Services | Resilient Slope LLC</title>
+                <meta name="description" content="Explore our range of services offered." />
+            </Helmet>
             <Title title="Services"/>
-            <div className='w-10/12'>
-                <h1>Test</h1>
+            <div className='grid grid-cols-1 sm:grid-cols-3 gap-4 mx-5 mt-5'>
+                {data.map((service, index) => (
+                    <div data-aos="zoom-in-up" data-aos-offset={service.start} data-aos-easing="ease-out" data-aos-duration={service.end} key={index} className="card bg-base-100 shadow-xl border border-inherit">
+                        <div className="card-body">
+                            <h2 className="text-blue-500 font-Georgia text-2xl card-title hover:font-serif">{service.title}</h2>
+                            <ul className="space-y-2">
+                                {service.category_array.map((category, i) => (
+                                     <li key={i}><span className="dot"></span>{category}</li>
+                                ))}
+                            </ul>
+                        </div>
+                        <figure>
+                            <img className='h-64 w-full' src={service.image_url} alt={service.title} />
+                        </figure>
+                    </div>
+                ))}
             </div>
         </div>
     );
